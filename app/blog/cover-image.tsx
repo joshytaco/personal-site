2428ba77601df04f46a903221a0dda4f297f8bf0
Josh Hooton <hooton.josh@gmail.com>
2023-12-29 21:41:11 -0600

    Update formatting of blog home page

--- app/blog/cover-image.tsx
+++ app/blog/cover-image.tsx
@@ -20,9 +20,9 @@ export default function CoverImage({
       priority
       width={2000}
       height={1000}
-      className={cn('shadow-small', {
-        'hover:shadow-medium transition-shadow duration-200': slug,
-      })}
+      // className={cn('shadow-small', {
+      //   'hover:shadow-medium transition-shadow duration-200': slug,
+      // })}
       src={url}
     />
   );
